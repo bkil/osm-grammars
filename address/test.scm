19b582e716ef84d6d061826da1d2f47b3743736e
bkil <b.K.il.h.u+tigbuh@gmail.com>
2020-08-22 00:01:21 +0200

    address.waxeye: pivot HouseNumber vs. House in AST

--- address/test.scm
+++ address/test.scm
@@ -5,13 +5,13 @@
 (Addr (Postcode #\2#\4#\8#\3) (CTownStreet (CTown #\G#\á#\r#\d#\o#\n#\y) (CStreet #\B#\a#\l#\a#\t#\o#\n#\i)) (Type #\u#\t#\c#\a) (House (HouseNumber #\1 #\/ #\a) (LevelN #\2) (DoorN #\3)) (Locator #\V#\2#\ #\ü#\z#\l#\e#\t))
 
 "Őr Fő út 9. 2956/3 hrsz"
-(Addr (CTownStreet (CTown #\Ő#\r) (CStreet #\F#\ő)) (Type #\ú#\t) (Number (House #\9) (ConscriptionHrsz #\2#\9#\5#\6#\/#\3)))
+(Addr (CTownStreet (CTown #\Ő#\r) (CStreet #\F#\ő)) (Type #\ú#\t) (Number (HouseNumber #\9) (ConscriptionHrsz #\2#\9#\5#\6#\/#\3)))
 
 "Őr Fő út (2956/3 hrsz) 9."
-(Addr (CTownStreet (CTown #\Ő#\r) (CStreet #\F#\ő)) (Type #\ú#\t) (Number (ConscriptionHrsz #\2#\9#\5#\6#\/#\3) (House #\9)))
+(Addr (CTownStreet (CTown #\Ő#\r) (CStreet #\F#\ő)) (Type #\ú#\t) (Number (ConscriptionHrsz #\2#\9#\5#\6#\/#\3) (HouseNumber #\9)))
 
 "Őr Május 1 utca 2"
-(Addr (CTownStreet (CTown #\Ő#\r) (CStreet #\M#\á#\j#\u#\s#\ #\1)) (Type #\u#\t#\c#\a) (House #\2))
+(Addr (CTownStreet (CTown #\Ő#\r) (CStreet #\M#\á#\j#\u#\s#\ #\1)) (Type #\u#\t#\c#\a) (HouseNumber #\2))
 
 "Őr 7-es főút 42. km"
 (Addr (CTownStreet (CTown #\Ő#\r) (CStreet #\7#\-#\e#\s)) (Type #\f#\ő#\ú#\t) (Km #\4#\2))
